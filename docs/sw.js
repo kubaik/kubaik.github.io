e6d581e72bb79b8d2c080a2e032862b5a33a5ca6
kubai <kevkubai@gmail.com>
2026-05-15 13:43:10 +0300

    delete deduplicate

--- docs/sw.js
+++ docs/sw.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'v1778838121';
+const CACHE_VERSION = 'v1778841774';
 const CACHE_SHELL = `shell-${CACHE_VERSION}`;
 const CACHE_PAGES = `pages-${CACHE_VERSION}`;
 const CACHE_IMAGES = `images-${CACHE_VERSION}`;
