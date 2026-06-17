414546e726c9ec86321d34c8dda59f874136a0d7
kubai <kevkubai@gmail.com>
2026-06-17 11:32:14 +0300

    retesting

--- docs/sw.js
+++ docs/sw.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'v1781685042';
+const CACHE_VERSION = 'v1781685123';
 const CACHE_SHELL = `shell-${CACHE_VERSION}`;
 const CACHE_PAGES = `pages-${CACHE_VERSION}`;
 const CACHE_IMAGES = `images-${CACHE_VERSION}`;
