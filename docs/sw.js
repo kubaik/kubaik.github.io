84e1ee7cba5e85d4d5dec1d07f6bc01781188f13
kubai <kevkubai@gmail.com>
2026-07-02 14:59:48 +0300

    update robots .txt

--- docs/sw.js
+++ docs/sw.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'v1782992287';
+const CACHE_VERSION = 'v1782993463';
 const CACHE_SHELL = `shell-${CACHE_VERSION}`;
 const CACHE_PAGES = `pages-${CACHE_VERSION}`;
 const CACHE_IMAGES = `images-${CACHE_VERSION}`;
