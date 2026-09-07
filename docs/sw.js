8890209240f377fc6d87a1471bcc641ee7800f01
kevinweru <kevin.weru@mgas.ke>
2026-09-07 12:48:34 +0300

    update tweet

--- docs/sw.js
+++ docs/sw.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'v1788771501';
+const CACHE_VERSION = 'v1788774502';
 const CACHE_SHELL = `shell-${CACHE_VERSION}`;
 const CACHE_PAGES = `pages-${CACHE_VERSION}`;
 const CACHE_IMAGES = `images-${CACHE_VERSION}`;
