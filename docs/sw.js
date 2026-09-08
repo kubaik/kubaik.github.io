cc667ec78163064f0ae1df9886bcb5969117329b
kevinweru <kevin.weru@mgas.ke>
2026-09-08 11:56:19 +0300

    retesting deployment

--- docs/sw.js
+++ docs/sw.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'v1788857226';
+const CACHE_VERSION = 'v1788857747';
 const CACHE_SHELL = `shell-${CACHE_VERSION}`;
 const CACHE_PAGES = `pages-${CACHE_VERSION}`;
 const CACHE_IMAGES = `images-${CACHE_VERSION}`;
