0ba4a44b2b1f92c683f5e2dd55673908c23e73fa
kevinweru <kevin.weru@mgas.ke>
2026-09-19 14:32:50 +0300

    testing improve classify_posts

--- docs/sw.js
+++ docs/sw.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'v1789810451';
+const CACHE_VERSION = 'v1789817529';
 const CACHE_SHELL = `shell-${CACHE_VERSION}`;
 const CACHE_PAGES = `pages-${CACHE_VERSION}`;
 const CACHE_IMAGES = `images-${CACHE_VERSION}`;
