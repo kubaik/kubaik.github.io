0bf8dad7687867ebb1346b2daeb7d8a6ae0c6a41
kevinweru <kevin.weru@mgas.ke>
2026-09-21 14:30:52 +0300

    Adsense removed

--- docs/sw.js
+++ docs/sw.js
@@ -13,7 +13,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'v1789984937';
+const CACHE_VERSION = 'v1789990166';
 const CACHE_SHELL = `shell-${CACHE_VERSION}`;
 const CACHE_PAGES = `pages-${CACHE_VERSION}`;
 const CACHE_IMAGES = `images-${CACHE_VERSION}`;
